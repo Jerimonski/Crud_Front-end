877f97c332ef4a0e469acbb2bf4ddeceecddb224
Bangvolver <alikatormes@gmail.com>
2025-04-09 16:04:41 -0400

    añadido acceso a usuariosGET

--- src/pages/Home/components/WelcomeToSection/WelcomeToSection.jsx
+++ src/pages/Home/components/WelcomeToSection/WelcomeToSection.jsx
@@ -1,5 +1,28 @@
-import React from "react"
+import { useEffect } from "react";
+import { useState } from "react";
+import { axios } from "axios";
 
 export default function WelcomeToSection() {
-  return <div className='text-lg'>WelcomeToSection</div>
+  const [user, setUser] = useState([]);
+
+  const dataFetch = () => {
+    return axios
+      .get("http://localhost:3000/usuarios/")
+      .then((response) => setUser(response.data));
+  };
+
+  useEffect(() => {
+    dataFetch();
+  }, []);
+
+  return (
+    <div className='text-lg'>
+      WelcomeToSection
+      <h1 className=''>Hola mundo!</h1>
+      {user &&
+        user.map((user, index) => {
+          return <div key={index}>{user.name}</div>;
+        })}
+    </div>
+  );
 }
