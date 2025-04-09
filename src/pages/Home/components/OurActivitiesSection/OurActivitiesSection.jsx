cd486b60230ca888218aad25f25c993053cd56d9
jeri <jerimonski@gmail.com>
2025-04-09 17:32:19 -0400

    style: new Card Style

--- src/pages/Home/components/OurActivitiesSection/OurActivitiesSection.jsx
+++ src/pages/Home/components/OurActivitiesSection/OurActivitiesSection.jsx
@@ -1,28 +1,41 @@
 import React from "react"
 import Cards from "./components/Cards"
 
-
 export default function OurActivitiesSection() {
-  return <div className='text-lg'>{/* Título y Descripción */}
-   
-  <h2 className="text-3xl font-bold">Our Activities</h2>
-  <p className="text-gray-700 mt-2 font-bold">
-  ¡Vive la experiencia! Reserva ya entre nuestras mejores clases y actividades deportivas.
-  </p>
-
-  {/* Contenedor de Tarjetas */}
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-8 px-6">
-    {/* Tarjetas de Actividad */}
-     <Cards title={"TENIS"} text={"Ven a reservar nuestras canchas profesionales para tus encuentros con amigos.🎾😊"}/>
-     <Cards title={"YOGA"} text={"Únase a nuestras clases de yoga dirigidas por expertos para todos los niveles de experiencia.🧘‍♂️🧘‍♀️"}/>
-     <Cards title={"GIMNASIO"} text={"Accede a equipos de última generación y sesiones de entrenamiento personal.💪🏋️‍♂️🏋️‍♀️"}/>
-     <Cards title={"NATACIÓN"} text={"Disfrute de nuestra piscina olímpica con carriles ademas de clases personalizadas.🏊"}/>
-  </div>
-
-  {/* Botón de Ver Más */}
-  <div className="text-center mt-6">
-    <button className="border font-bold border-gray-500 text-gray-800 px-4 py-2 hover:scale-110 hover:bg-emerald-300  duration-300 rounded-lg">
-      View All Activities
-    </button>
-  </div></div>
+  return (
+    <div className='text-lg bg-black text-white py-12'>
+      <div className='mx-auto w-16 border-t-4 border-off-green' />
+      <h2 className='text-3xl py-2 text-center font-bold'>Our Activities</h2>
+      <p className='text-secondary-text pb-4 text-center font-bold'>
+        ¡Vive la experiencia! Reserva ya entre nuestras mejores clases y
+        actividades deportivas.
+      </p>
+      <div className='flex justify-center gap-6 mt-8 px-6'>
+        <Cards
+          title={"TENIS"}
+          text={
+            "Ven a reservar nuestras canchas profesionales para tus encuentros con amigos.🎾😊"
+          }
+        />
+        <Cards
+          title={"YOGA"}
+          text={
+            "Únase a nuestras clases de yoga dirigidas por expertos para todos los niveles de experiencia.🧘‍♂️🧘‍♀️"
+          }
+        />
+        <Cards
+          title={"GIMNASIO"}
+          text={
+            "Accede a equipos de última generación y sesiones de entrenamiento personal.💪🏋️‍♂️🏋️‍♀️"
+          }
+        />
+        <Cards
+          title={"NATACIÓN"}
+          text={
+            "Disfrute de nuestra piscina olímpica con carriles ademas de clases personalizadas.🏊"
+          }
+        />
+      </div>
+    </div>
+  )
 }
--- src/pages/Home/components/OurActivitiesSection/components/Cards.jsx
+++ src/pages/Home/components/OurActivitiesSection/components/Cards.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react"
 
-export default function Cards({title, text}) {
+export default function Cards({ title, text }) {
   return (
-    <div className="bg-gray-700 p-6 rounded-lg  shadow-lg flex flex-col justify-between">
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <p className="text-gray-200 mt-2">
-        
-        {text}
-      </p>
-      <div className='flex justify-center'>
-      <button className="mt-4 bg-yellow-500 px-6 py-2 w-fit text-black font-semibold rounded hover:scale-110 duration-300">
-        Reservar Ahora 
-      </button>
+    <section className='bg-linear-to-b from-white to-black px-8 text-sm rounded-md shadow-lg flex flex-col justify-between h-[320px] max-w-[320px]'>
+      <div className='pt-36'>
+        <h3 className='text-lg text-white font-semibold'>{title}</h3>
+        <p className='text-secondary-text mt-2'>{text}</p>
+        <div className='flex justify-center'>
+          <button className='w-full mt-4 bg-light-green py-2 text-black font-semibold rounded hover:scale-110 duration-300'>
+            Reservar Ahora
+          </button>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
