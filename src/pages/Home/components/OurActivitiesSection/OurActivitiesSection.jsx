a1ddaf117acfb8edb16a7e717288061dd3792cbf
InZane017 <107491446+InZane017@users.noreply.github.com>
2025-04-10 11:36:48 -0400

    Arreglo del login y registro y un poco de front

--- src/pages/Home/components/OurActivitiesSection/OurActivitiesSection.jsx
+++ src/pages/Home/components/OurActivitiesSection/OurActivitiesSection.jsx
@@ -5,7 +5,7 @@ import Cards from "./components/Cards"
 export default function OurActivitiesSection() {
   return <div className='text-lg'>{/* Título y Descripción */}
    
-  <h2 className="text-3xl font-bold">Our Activities</h2>
+  <h2 className="text-3xl font-bold">Nuestras Actividades</h2>
   <p className="text-gray-700 mt-2 font-bold">
   ¡Vive la experiencia! Reserva ya entre nuestras mejores clases y actividades deportivas.
   </p>
@@ -21,7 +21,7 @@ export default function OurActivitiesSection() {
 
   {/* Botón de Ver Más */}
   <div className="text-center mt-6">
-    <button className="border font-bold border-gray-500 text-gray-800 px-4 py-2 hover:scale-110 hover:bg-emerald-300  duration-300 rounded-lg">
+    <button className="border font-bold border-gray-500 text-gray-800 px-4 py-2 hover:scale-110 hover:bg-yellow-400  duration-300 rounded-lg">
       View All Activities
     </button>
   </div></div>
