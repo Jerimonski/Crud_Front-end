4e529a5ac1fe55973e8bc5eb72c12fb4900c0c51
jeri <jerimonski@gmail.com>
2025-04-22 18:52:40 -0400

    base

---
+++ src/pages/Sports/Sports.jsx
@@ -0,0 +1,12 @@
+import SectionSport from "./components/SectionSport"
+export default function Sports() {
+  return (
+    <div className='h-screen'>
+      <SectionSport></SectionSport>
+      <section>Yoga</section>
+      <section>Ginmasio</section>
+      <section>Tennis</section>
+      <section>Natacion</section>
+    </div>
+  )
+}
--- src/routes/RoutesConfig.jsx
+++ src/routes/RoutesConfig.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home/Home"
 import Contact from "../pages/ContactPage/Contact"
 import AboutUs from "../pages/AboutUs/AboutUs"
 import SignIn_LogInPage from "./../pages/SignIn-LogInPage/SignIn-LogInPage"
+import Sports from "../pages/Sports/Sports"
 
 export default function RoutesConfig() {
   return (
@@ -11,6 +12,7 @@ export default function RoutesConfig() {
       <Route path='/home' element={<Home />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/aboutUs' element={<AboutUs />} />
+      <Route path='/deportes' element={<Sports />} />
       <Route path='/signIn-logIn' element={<SignIn_LogInPage />} />
     </Routes>
   )
