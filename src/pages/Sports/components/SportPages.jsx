e56035d7a5874fed3230cdf58318f559b98c8fad
jeri <jerimonski@gmail.com>
2025-05-17 17:11:05 -0400

    test 4

--- src/pages/Sports/components/SportPages.jsx
+++ src/pages/Sports/components/SportPages.jsx
@@ -28,7 +28,6 @@ export default function SportPages() {
       nombre: user.nombre,
       contenido: nuevoComentario,
       fecha_publicacion: new Date().toISOString(),
-      deporte,
     })
     setNuevoComentario("")
   }
